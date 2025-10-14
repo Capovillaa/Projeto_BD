09da026a3c7d33485c9c02cf5e4288fb1aca991f
HenriqueMartins2502 <henrique.dgam@puccampinas.edu.br>
2025-10-14 16:23:18 -0300

    Alterações feitas

--- classes/user.py
+++ classes/user.py
@@ -26,3 +26,33 @@ class User:
         except Exception as e:
             print(f"Ocorreu um erro ao cadastrar o usuário: {e}")
             return False
+        
+    def login(cls, db, email, senha):
+        if db is None:
+            print("Erro: A instância do banco de dados é inválida para login.")
+            return None
+        
+        try:
+            users_collection = db['users']
+        
+            usuario_doc = users_collection.find_one({"email": email})
+            
+            if usuario_doc:
+                if usuario_doc['senha'] == senha:
+                    print(f"Login bem-sucedido!")
+                    
+                    return cls(
+                        email=usuario_doc['email'], 
+                        senha=usuario_doc['senha'], 
+                        nickname=usuario_doc['nickname']
+                    )
+                else:
+                    print("Erro: Senha incorreta.")
+                    return None
+            else:
+                print(f"Erro: Usuário com o email '{email}' não encontrado.")
+                return None
+                
+        except Exception as e:
+            print(f"Ocorreu um erro durante o login: {e}")
+            return None
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from database.mongo_connection import MongoConnector
 from connection_string import URI
 from classes.user import User
+
 def main():
     
     DB_NAME = "Mensageria"
@@ -11,30 +12,64 @@ def main():
         return    
     
     db = connection.getDataBase()
+    
+    usuario_logado = None 
 
     while True:
         print("\n--- Sistema de Mensagem ---")
-        print("1. Login")
-        print("2. Criar Conta")
-        print("3. Sair")
+        
+        if usuario_logado:
+            print(f"Bem-vindo, {usuario_logado.nickname}!")
+            print("1. Enviar Mensagem")
+            print("2. Deslogar")
+            print("3. Sair")
+        else:
+            print("1. Login")
+            print("2. Criar Conta")
+            print("3. Sair")
         
         escolha = input("Escolha uma opção: ")
         
-        if escolha == '1':
-            print("Funcionalidade de Login ainda não implementada.")
+        if not usuario_logado: 
+            if escolha == '1':
+                print("\n--- Login de Usuário ---")
+                email = input("Digite seu e-mail: ")
+                senha = input("Digite sua senha: ")
                 
-        elif escolha == '2':
-            email = input("Digite seu endereco de e-mail: ")
-            nickname = "@"+ input("Digite o seu nome de usuário: ")
-            senha = input("Digite sua senha: ")
-            currentUser = User(email,senha,nickname)
-            currentUser.cadastro(db)
-        elif escolha == '3':
-            print("Saindo do programa. Até mais!")
+                usuario_logado = User.login(db, email, senha) 
+                
+                if usuario_logado:
+                    print(f"Você está agora logado como {usuario_logado.nickname}.")
+                    
+            elif escolha == '2':
+                print("\n--- Criação de Conta ---")
+                email = input("Digite seu endereco de e-mail: ")
+                nickname = "@"+ input("Digite o seu nome de usuário: ")
+                senha = input("Digite sua senha: ")
+                currentUser = User(email,senha,nickname)
+                currentUser.cadastro(db)
+                
+            elif escolha == '3':
+                print("Saindo do programa. Até mais!")
+                break
             
-            break
-        else:
-            print("Opção inválida. Por favor, tente novamente.")
+            else:
+                print("Opção inválida. Por favor, tente novamente.")
+                
+        else: 
+            if escolha == '1':
+                print("")
+                
+            elif escolha == '2':
+                print(f"Deslogando de {usuario_logado.nickname}.")
+                usuario_logado = None 
+                
+            elif escolha == '3':
+                print("Saindo do programa. Até mais!")
+                break
+            
+            else:
+                print("Opção inválida. Por favor, tente novamente.")
 
-if __name__ == "__main__":
+if __name__ == "_main_":
     main()
