1b8cd5b57507700294aaf29760921638dac2030e
Capovillaa <gabrielcapovilla2004@gmail.com>
2025-10-15 12:26:14 -0300

    Alterações na classe de segurança agora funçoes pertencem a classe security

---
+++ src/classes/security.py
@@ -0,0 +1,45 @@
+import base64
+import os
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+class Security:
+    
+    SALT_SIZE = 16
+
+    @classmethod
+    def kdf(cls,senha: bytes, salt: bytes ) -> bytes:
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=1_200_000,
+        )
+
+        key = base64.urlsafe_b64encode(kdf.derive(senha))
+        return key
+    
+    @classmethod
+    def encrypt(cls,senha: bytes, mensagem: bytes) -> bytes:
+
+        salt = os.urandom(cls.SALT_SIZE)
+
+        key = cls.kdf(senha,salt)
+        f = Fernet(key)
+
+        token = f.encrypt(mensagem)
+
+        return salt + token
+    
+    @classmethod
+    def decrypt(cls,senha: bytes, mensagem_criptografada: bytes) -> bytes:
+
+        salt = mensagem_criptografada[:cls._SALT_SIZE]
+        token = mensagem_criptografada[cls._SALT_SIZE:]
+
+        key = cls.kdf(senha,salt)
+        f = Fernet(key)
+
+        mensagem_descriptografada = f.decrypt(token)
+        return mensagem_descriptografada
