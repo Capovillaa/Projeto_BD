d517e2dd4b5cfe7e7acfa669bed72e51f240f984
HenriqueMartins2502 <henrique.dgam@puccampinas.edu.br>
2025-10-15 19:41:41 -0300

    adição do metodo verificar_usuario_existe, listar_mensganes_nao_lidas e marcar_como_lida, métodos cruciais para o andamento do projeto

--- src/database/database_manager.py
+++ src/database/database_manager.py
@@ -41,4 +41,52 @@ class DatabaseManager:
         message_data = message_obj.to_dict()
         
         self.db['mensagens'].insert_one(message_data)
-       
+
+    def verificar_usuario_existe(self, nickname):        
+        try:
+            users_collection = self.db['users']
+            
+            usuario_doc = users_collection.find_one({"nickname": nickname})
+            return usuario_doc is not None
+            
+        except Exception as e:
+            print(f"Erro ao buscar usuário '{nickname}': {e}")
+            return False 
+
+    def listar_mensagens_nao_lidas(self, user_logado):
+        try:
+            mensagens_collection = self.db['mensagens']
+            
+            query = {
+                "to": user_logado,
+                "status": "nao lida"
+            }
+            
+            mensagens_nao_lidas = list(mensagens_collection.find(query))
+            
+            return mensagens_nao_lidas
+            
+        except Exception as e:
+            print(f"Erro ao listar mensagens não lidas: {e}")
+            return []
+        
+
+    def marcar_como_lida(self, message_id): 
+        try:
+            mensagens_collection = self.db['mensagens']
+            
+            query = {"_id": message_id}
+            
+            update = {"$set": {"status": "lida"}}
+            
+            result = mensagens_collection.update_one(query, update)
+            
+            if result.matched_count == 1:
+                return True
+            else:
+                print(f"Aviso: Não foi possível encontrar a mensagem com ID {message_id}.")
+                return False
+                
+        except Exception as e:
+            print(f"Erro ao marcar mensagem como lida: {e}")
+            return False
